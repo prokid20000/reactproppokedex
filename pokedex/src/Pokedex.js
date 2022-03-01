c4748fe2a11664d5c17fc9c8f13d8de96d997a08
stevtang <stevenmtang1@gmail.com>
2022-02-28 16:08:55 -0800

    Completed pokedex, testing needed

--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -1,1 +1,20 @@
-import React from 'react';
+import React from "react";
+import Pokecard from "./Pokecard";
+
+function Pokedex(props) {
+  return (
+    <div>
+      {props.map((p) => (
+        <Pokecard
+          id={p.id}
+          name={p.name}
+          type={p.type}
+          exp={p.base_experience}
+        />
+      ))}
+      ;
+    </div>
+  );
+}
+
+export default Pokedex;
